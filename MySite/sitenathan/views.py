f79e3e3fc4e5e744781cb87b9491feb339a0879c
= <=>
2022-12-10 19:03:23 +0100

    added IA

--- MySite/sitenathan/views.py
+++ MySite/sitenathan/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from sitenathan import forms
 # Create your views here.
 
-
+from sitenathan.ai_funcs import tweet_prediction
 
 def homepage(request):
     return render(request,'homepage.html')
@@ -13,3 +13,27 @@ def about_me(request):
 
 def front(request):
     return render(request,"front.html")
+
+
+def parcours(request):
+    return render(request,"mon-parcours.html")
+
+
+def tweet(request):
+    if request.method=="GET":
+        form  = forms.TweetForm()
+        context = {"form":form,"bar_position":-25}
+        return render(request,"tweet.html",context=context)
+
+    if request.method=="POST":
+        form  = forms.TweetForm(request.POST)
+        if form.is_valid():
+            message = form.cleaned_data.get("text")
+            value_predicted = tweet_prediction(message)
+            bar_position = int(50*value_predicted[0][0]-50)
+            with open('/home/nathan/f.txt','a') as f:
+                f.write(str(value_predicted[0][0]))
+            context = {"bar_position":bar_position,"form":form}
+
+        return render(request,'tweet.html',context=context)
+        
